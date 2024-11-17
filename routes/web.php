5b2e8ea4d01aa981d4b410f69409eb1a9f68cb03
aoki-cloud <takosukeaoki@icloud.com>
2024-11-17 14:06:45 +0900

    管理者側の基本設定機能

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin;
 use App\Http\Controllers\Admin\HelloController;
 use App\Http\Controllers\Admin\RestaurantController;
+use App\Http\Controllers\Admin\CompanyController;
+use App\Http\Controllers\Admin\TermController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,15 +18,27 @@ use App\Http\Controllers\Admin\RestaurantController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'guest:admin'], function () {
+    Route::get('home', [Admin\HomeController::class, 'index'])->name('home');
+    Route::resource('users', Admin\UserController::class)->only(['index', 'show']);
+    Route::resource('restaurants', Admin\RestaurantController::class);
+    Route::resource('categories', Admin\CategoryController::class);
+    Route::get('company', [CompanyController::class, 'index'])->name('company.index');
+    Route::get('terms', [TermController::class, 'index'])->name('terms.index');
 });
 
 require __DIR__.'/auth.php';
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'auth:admin'], function () {
+    
+});
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'auth:admin'], function () {
     Route::get('home', [Admin\HomeController::class, 'index'])->name('home');
     Route::resource('users', Admin\UserController::class)->only(['index', 'show']);
     Route::resource('restaurants', Admin\RestaurantController::class);
-    Route::resource('categories', Admin\CategoryController::class);
+    Route::resource('categories', Admin\CategoryController::class)->only(['index', 'store', 'update', 'destroy']);
+    Route::resource('company', Admin\CompanyController::class)->only(['index', 'edit', 'update']);
+    Route::resource('terms', Admin\TermController::class)->only(['index', 'edit', 'update']);
+
 });
