5979724d9e36c63dfb667ad039fb31291d0e028d
aoki-cloud <takosukeaoki@icloud.com>
2024-11-23 11:16:48 +0900

    会員側の店舗詳細ページ

--- tests/Feature/RestaurantTest.php
+++ tests/Feature/RestaurantTest.php
@@ -41,4 +41,39 @@ class RestaurantTest extends TestCase
         $response = $this->actingAs($admin, 'admin')->get(route('restaurants.index'));
         $response->assertRedirect(route('admin.home')); // 管理者リダイレクト先
     }
+
+    //未ログインのユーザーは会員側の店舗詳細ページにアクセスできる
+    public function test_guest_can_access_restaurants_show()
+    {
+        $restaurant = Restaurant::factory()->create();
+
+        $response = $this->get(route('restaurants.show', $restaurant));
+        $response->assertStatus(200);
+    }
+
+    //ログイン済みの一般ユーザーは会員側の店舗詳細ページにアクセスできる
+    public function test_general_user_can_access_restaurants_show()
+    {
+        $user = User::factory()->create();
+
+        $restaurant = Restaurant::factory()->create();
+
+        $response = $this->actingAs($user)->get(route('restaurants.show', $restaurant));
+        $response->assertStatus(200);
+    }
+
+    // ログイン済みの管理者は会員側の店舗詳細ページにアクセスできない
+    public function test_admin_cannot_access_restaurants_show()
+    {
+        $admin = new Admin();
+        $admin->email = 'admin@example.com';
+        $admin->password = Hash::make('nagoyameshi');
+        $admin->save();
+ 
+        $restaurant = Restaurant::factory()->create();
+ 
+        $response = $this->actingAs($admin, 'admin')->get(route('restaurants.show', $restaurant));
+ 
+        $response->assertRedirect(route('admin.home'));
+    }
 }
