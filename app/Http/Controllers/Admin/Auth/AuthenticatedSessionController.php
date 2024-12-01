e1ace64b7102948a4a6f1c56554b1ab6d6c4a3e1
aoki-cloud <takosukeaoki@icloud.com>
2024-12-01 10:06:10 +0900

    管理者側のトップページ

--- app/Http/Controllers/Admin/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Admin/Auth/AuthenticatedSessionController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers\Admin\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
@@ -13,36 +13,36 @@ use Illuminate\View\View;
 class AuthenticatedSessionController extends Controller
 {
     /**
-     * Display the login view.
-     */
-    public function create(): View
-    {
-        return view('auth.login');
-    }
-
-    /**
-     * Handle an incoming authentication request.
-     */
-    public function store(LoginRequest $request): RedirectResponse
-    {
-        $request->authenticate();
-
-        $request->session()->regenerate();
-
-        return redirect()->intended(RouteServiceProvider::HOME)->with('flash_message', 'ログインしました。');
-    }
-
-    /**
-     * Destroy an authenticated session.
-     */
-    public function destroy(Request $request): RedirectResponse
-    {
-        Auth::guard('web')->logout();
-
-        $request->session()->invalidate();
-
-        $request->session()->regenerateToken();
-
-        return redirect('/')->with('flash_message', 'ログアウトしました。');
-    }
+      * Display the login view.
+      */
+      public function create(): View
+      {
+          return view('admin.auth.login');
+      }
+  
+      /**
+       * Handle an incoming authentication request.
+       */
+      public function store(LoginRequest $request): RedirectResponse
+      {
+          $request->authenticate();
+  
+          $request->session()->regenerate();
+  
+          return redirect()->intended(RouteServiceProvider::ADMIN_HOME)->with('flash_message', 'ログインしました。');
+      }
+  
+      /**
+       * Destroy an authenticated session.
+       */
+      public function destroy(Request $request): RedirectResponse
+      {
+          Auth::guard('admin')->logout();
+  
+          $request->session()->invalidate();
+  
+          $request->session()->regenerateToken();
+  
+          return redirect('/')->with('flash_message', 'ログアウトしました。');
+      }
 }
